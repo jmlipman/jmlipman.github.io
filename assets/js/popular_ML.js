575772a71f4e0107b129a452418c976478aa9b49
Juan-Miguel Valverde <regjmlipman@hotmail.com>
2020-02-09 16:30:12 +0200

    Update popular_ML.js

--- assets/js/popular_ML.js
+++ assets/js/popular_ML.js
@@ -9,9 +9,9 @@ function loadRows(source, latest) {
       if ((data != null) && (data.length > 0)) {
           for (var i=0; i<data.length; i++) {
               row_content = '<div class="row">';
-              row_content += '<div class="col-2 themed-grid-col data-source"><table width="100%" height="100%" cellspacing="0", cellpadding="0"><tr><td align="center">'+data[i].source+'</td></tr></table></div>';
+              row_content += '<div class="col-2 themed-grid-col data-source"><table width="100%" height="100%" cellspacing="0" cellpadding="0"><tr><td align="center">'+data[i].source+'</td></tr></table></div>';
               row_content += '<div class="col-7 themed-grid-col data-title"><a class="row-title" href="'+data[i].url+'" target="_blank">'+data[i].title+'</a></div>';
-              row_content += '<div class="col-3 themed-grid-col data-author"><table width="100%" height="100%" cellspacing="0", cellpadding="0"><tr><td align="center"><a class="row-author" href="'+data[i].tweet_url+'" target="_blank">@'+data[i].author+'</a></td></tr></table></div>';
+              row_content += '<div class="col-3 themed-grid-col data-author"><table width="100%" height="100%" cellspacing="0" cellpadding="0"><tr><td align="center"><a class="row-author" href="'+data[i].tweet_url+'" target="_blank">@'+data[i].author+'</a></td></tr></table></div>';
               row_content += '</div>'
               $("div.rows-here").append(row_content);
           }
