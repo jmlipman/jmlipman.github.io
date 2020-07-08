9326b0561d2e64181d50e943e7e1a9403bb1b1fb
Juan-Miguel Valverde <regjmlipman@hotmail.com>
2020-07-08 22:17:33 +0300

    Update popular_ML.js

--- assets/js/popular_ML.js
+++ assets/js/popular_ML.js
@@ -11,18 +11,13 @@ function loadRows(source, latest) {
               row_content += '<div class="col-7 themed-grid-col data-title"><a class="row-title" href="'+data[i].url+'" target="_blank">'+data[i].url_title+'</a></div>';
               row_content += '<div class="col-3 themed-grid-col data-author"><table width="100%" height="100%" cellspacing="0" cellpadding="0"><tr><td align="center"><a class="row-author" href="https://twitter.com/'+data[i].tweet_screenname+'/status/'+data[i].tweet_id+'" target="_blank">@'+data[i].tweet_screenname+'</a></td></tr></table></div>';
               row_content += '</div>'*/
-              row_content = '<div class="row">';
-              row_content += '<div class="col-2 themed-grid-col data-source"><table width="100%" height="100%" cellspacing="0" cellpadding="0"><tr><td align="center">'+data[i].url_type+'</td></tr></table></div>';
-              row_content += '<div class="col-7 themed-grid-col data-title"><a class="row-title" href="'+data[i].url+'" target="_blank">'+data[i].url_title+'</a></div>';
-              row_content += '<div class="col-3 themed-grid-col data-author"><table width="100%" height="100%" cellspacing="0" cellpadding="0"><tr><td align="center"><a class="row-author" href="https://twitter.com/'+data[i].tweet_screenname+'/status/'+data[i].tweet_id+'" target="_blank">@'+data[i].tweet_screenname+'</a></td></tr></table></div>';
-              row_content += '</div>'
 
               row_content = '<div class="row">';
               row_content += '<div class="col-12 themed-grid-col data-source">';
-              row_content += '<table width="100%" height="100%" cellspacing="0" cellpadding="0">';
+              row_content += '<table class="popularml" width="100%" height="100%" cellspacing="0" cellpadding="0">';
               row_content += '<tr><td>';
 
-              row_content += '<table width="100%" height="100%" cellspacing="0" cellpadding="0">';
+              row_content += '<table class="popularml" width="100%" height="100%" cellspacing="0" cellpadding="0">';
               row_content += '<tr><td width="50%" align="left">';
               row_content += '<a class="row-author" href="https://twitter.com/'+data[i].tweet_screenname+'/status/'+data[i].tweet_id+'" target="_blank">@'+data[i].tweet_screenname+'</a>';
               row_content += '</td>';
@@ -35,7 +30,7 @@ function loadRows(source, latest) {
               row_content += '<a class="row-title" href="'+data[i].url+'" target="_blank">'+data[i].url_title+'</a>';
               row_content += '</td></tr>';
               row_content += '<tr><td>';
-              row_content += '<table width="100%" height="100%" cellspacing="0" cellpadding="0">';
+              row_content += '<table class="popularml" width="100%" height="100%" cellspacing="0" cellpadding="0">';
               row_content += '<tr><td width="50%" align="left">';
               row_content += '<a class="row-author" href="https://twitter.com/'+data[i].tweet_screenname+'/status/'+data[i].tweet_id+'" target="_blank">@'+data[i].tweet_screenname+'</a>';
               row_content += '</td>';
